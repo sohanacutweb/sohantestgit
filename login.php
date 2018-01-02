eba711274da052640c5c7abbc837756d37459e19
ashu306 <ashu.acutweb@gmail.com>
2018-01-02 18:35:44 +0530

    fisrt commit

--- login.php
+++ login.php
@@ -1,3 +1,13 @@
-<?php
-echo 'llll';
-?>
+<form name="loginForm" id="loginForm" method="POST">
+<input type="hidden" name="action" value="user_login">
+<div class="form-group">
+    <label for="email">User Name:</label>
+    <input type="text" class="form-control" name="username" id="username">
+  </div>
+  <div class="form-group">
+    <label for="pass">Password:</label>
+    <input type="password" class="form-control" name="password" id="password">
+  </div>
+   <button type="submit" name="login_button" id="login_button" class="login" data-loading-text="Loading---"> LOGIN 
+			</button>
+  </form>
