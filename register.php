a8d937581ea893ee80139fa2327f1610c1975805
sohanacutweb <sohan.acutweb@gmail.com2>
2018-01-02 17:20:20 +0530

    desing part

--- register.php
+++ register.php
@@ -1,3 +1,29 @@
 <?php
 require_once("config.php");
 ?>
+<!DOCTYPE html>
+<html>
+<body>
+<form action="/action_page.php">
+  First name:<br>
+  <input type="text" name="firstname" required />
+  <br>
+  Last name:<br>
+  <input type="text" name="lastname" required />
+  <br>
+  E-mail:<br>
+  <input type="text" name="email_addres" required />
+  <br>
+  Username:<br>
+  <input type="text" name="user_name" required />
+  <br>
+  Password:<br>
+  <input type="password" name="pass_word" required />
+  <br><br>
+  <input type="submit" value="Submit">
+</form> 
+
+<p>If you click the "Submit" button, the form-data will be sent to a page called "/action_page.php".</p>
+
+</body>
+</html>
